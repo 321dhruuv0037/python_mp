0e0365872f5f261115c17a1c9d296183d37ec9eb
Yashas Khot <321yashas0028@dbit.in>
2023-03-07 18:35:47 +0530

    customized line chart

--- linechart/linech.py
+++ linechart/linech.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 df = pd.read_csv('slider.csv')
 max_df = df.groupby(['year', 'stream'])['jee_perc'].mean().reset_index()
-fig = px.line(max_df, x='year', y='jee_perc', color='stream', title='Average Percentile by Year and Stream')
+fig = px.line(max_df, x='year', y='jee_perc', color='stream', title='Average Percentile by Year and Stream', markers=True, symbol="stream")
 
 fig.show()
